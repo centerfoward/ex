f57a3189379624af3362ed44cd17df1cc5c18f15
centerfoward <jsu5438@naver.com>
2024-04-09 16:41:30 +0900

    bug:메인액티비오류수정

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -24,7 +24,6 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    ActionBar abar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,42 +31,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.android);
-
-        abar = getSupportActionBar();
-
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                abar.setLogo(R.drawable.home);
-                abar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_USE_LOGO);
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int curld = item.getItemId();
-        switch (curld) {
-            case R.id.menu_refresh:
-                Toast.makeText(this, "새로고침 메뉴가 선택되었습니다", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.menu_search:
-                Toast.makeText(this, "검색 메뉴가 선택되었습니다", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.menu_settings:
-                Toast.makeText(this, "설정 메뉴가 선택됨", Toast.LENGTH_SHORT).show();
-                break;
-            default:
-                break;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
